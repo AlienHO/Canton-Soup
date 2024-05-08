db9aaafeefdd64bb216ffccffc3f40488c0edb71
Alien <hoalien0716@gmail.com>
2024-05-08 14:19:04 +0800

    /Canton-Soup/vue-ECharts/

--- vue-ECharts/vite.config.js
+++ vue-ECharts/vite.config.js
@@ -4,7 +4,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
-  base: '/Canton-Soup/vue-chart/',  // 设置为 GitHub 项目子路径
+  base: '/Canton-Soup/vue-ECharts/',  // 设置为 GitHub 项目子路径
   plugins: [
     vue(),
     Components({
